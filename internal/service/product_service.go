c4ed4dcd17509207dac9d0e315ce73a2fb2a6bb5
maycon silva <maycon8609@gmail.com>
2024-02-02 22:34:48 -0300

    feat: create product service

---
+++ internal/service/product_service.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"github.com/maycon8609/fullcycle-17-go-api/internal/database"
+	"github.com/maycon8609/fullcycle-17-go-api/internal/entity"
+)
+
+type ProductService struct {
+	ProductDB database.ProductDb
+}
+
+func NewProductService(productDB database.ProductDb) *ProductService {
+	return &ProductService{ProductDB: productDB}
+}
+
+func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProducts()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func (ps *ProductService) GetProduct(id string) (*entity.Product, error) {
+	product, err := ps.ProductDB.GetProduct(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (ps *ProductService) GetProductsByCategoryID(categoryID string) ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProductsByCategoryID(categoryID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func (ps *ProductService) CreateProduct(name, description, category_id, image_url string, price float64) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, category_id, image_url, price)
+
+	_, err := ps.ProductDB.CreateProduct(product)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
