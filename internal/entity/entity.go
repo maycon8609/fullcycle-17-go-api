65df12b9f1d5172fa35c4e57501b0a09deb832bd
maycon silva <maycon8609@gmail.com>
2024-02-05 17:06:30 -0300

    refactor: updated keys to support json format

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -3,17 +3,17 @@ package entity
 import "github.com/google/uuid"
 
 type Category struct {
-	ID   string
-	Name string
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 type Product struct {
-	ID          string
-	Name        string
-	Description string
-	CategoryID  string
-	ImageURL    string
-	Price       float64
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	CategoryID  string  `json:"category_id"`
+	ImageURL    string  `json:"image_url"`
+	Price       float64 `json:"price"`
 }
 
 func NewCategory(name string) *Category {
